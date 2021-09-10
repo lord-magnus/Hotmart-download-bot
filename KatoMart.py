44c9da19e23a77348822ac8bb4a0d8b0081d216e
Lord Magnus <90481290+lord-magnus@users.noreply.github.com>
2021-09-11 00:44:00 +0200

    Introducing getPlayerInfo function

--- KatoMart.py
+++ KatoMart.py
@@ -235,6 +235,21 @@ def getInfoAula(dominio, url, hash):
 
     return infoAula
 
+def getPlayerInfo(authMart, media):
+    playerData = authMart.get(media['mediaSrcUrl']).text
+
+    # Descomentar para ver o que caralhos a plataforma retorna do player
+    # with open("player.html", "w") as phtml:
+    #     phtml.write(playerData)
+
+    playerDom = BeautifulSoup(playerData, features="html.parser")
+    playerConfigurations = playerDom \
+        .find(text=re.compile("window.playerConfig"))[:-1]
+    playerConfigJson = playerConfigurations.split(" ", 2)[2]
+    playerInfo = json.loads(playerConfigJson)
+
+    return playerInfo['player']
+
 def baixarCurso(authMart, infoCurso, downloadAll):
     TEMP_FOLDER = criaTempFolder()
     NOME_CURSO, PATH_CURSO = criaCurso(infoCurso['nome'])
@@ -290,7 +305,7 @@ def baixarCurso(authMart, infoCurso, downloadAll):
                 NOME_AULA, PATH_AULA = criaSubDir(PATH_MODULO, aula['pageOrder'], aula['name'])
 
                 print(f"{Colors.Magenta}Tentando baixar a aula: {Colors.Cyan}{NOME_MODULO}{Colors.Magenta}/{Colors.Green}{NOME_AULA}{Colors.Magenta}!{Colors.Reset}")
-                
+
                 lessonCount += 1
                 infoAula = getInfoAula(DOMINIO, URL, hash)
 
@@ -303,123 +318,117 @@ def baixarCurso(authMart, infoCurso, downloadAll):
                 while tryDL:
                     try:
                         try:
-                            for x, media in enumerate(infoAula['mediasSrc'], start=1):
-                                if media['mediaType'] == "VIDEO":
-                                    print(
-                                        f"\t{Colors.Magenta}Tentando baixar o vídeo {x}{Colors.Reset}")
-                                    aulagetter = authMart
-                                    playerData = aulagetter.get(
-                                        media['mediaSrcUrl']).text
-                                    # Descomentar para ver o que caralhos a plataforma retorna do player
-                                    # with open("player.html", "w") as phtml:
-                                    #     phtml.write(playerData)
-                                    playerInfo = json.loads(BeautifulSoup(playerData, features="html.parser").find(
-                                        text=re.compile("window.playerConfig"))[:-1].split(" ", 2)[2])
-                                    segVideos += playerInfo['player']['mediaDuration']
-                                    for asset in playerInfo['player']['assets']:
-                                        # TODO Melhorar esse workaround para nome longo
-                                        filePath = os.path.dirname(
-                                            os.path.abspath(__file__))
-                                        aulaPath = f"{filePath}/Cursos/{NOME_CURSO}/{NOME_MODULO}/{NOME_AULA}/aula-{x}.mp4"
-                                        if len(aulaPath) > 254:
-                                            if not os.path.exists(f"Cursos/{NOME_CURSO}/ev"):
-                                                os.makedirs(
-                                                    f"Cursos/{NOME_CURSO}/ev")
-                                            tempNM = ''.join(random.choices(
-                                                string.ascii_uppercase + string.digits, k=8))
-                                            with open(f"Cursos/{NOME_CURSO}/ev/list.txt", "a", encoding=ENCODING) as safelist:
-                                                safelist.write(
-                                                    f"{tempNM} = {NOME_CURSO}/{NOME_MODULO}/{NOME_AULA}/aula-{x}.mp4\n")
-                                            aulaPath = f"Cursos/{NOME_CURSO}/ev/{tempNM}.mp4"
-                                            videosLongos += 1
-                                        if not os.path.isfile(aulaPath):
-                                            videoData = aulagetter.get(
-                                                f"{asset['url']}?{playerInfo['player']['cloudFrontSignature']}")
-                                            masterPlaylist = m3u8.loads(
+                            for index, media in enumerate(infoAula['mediasSrc'], start=1):
+                                if media['mediaType'] != "VIDEO":
+                                    continue
+
+                                print(f"\t{Colors.Magenta}Tentando baixar o vídeo {index}{Colors.Reset}")
+
+                                playerInfo = getPlayerInfo(authMart, media)
+                                segVideos += playerInfo['mediaDuration']
+
+                                for asset in playerInfo['assets']:
+                                    # TODO Melhorar esse workaround para nome longo
+
+                                    aulaPath = os.path.join(PATH_AULA , "aula-{index}.mp4")
+                                    if len(aulaPath) > 254:
+                                        if not os.path.exists(f"Cursos/{NOME_CURSO}/ev"):
+                                            os.makedirs(f"Cursos/{NOME_CURSO}/ev")
+
+                                        tempNM = ''.join(random.choices(string.ascii_uppercase + string.digits, k=8))
+
+                                        with open(f"Cursos/{NOME_CURSO}/ev/list.txt", "a", encoding=ENCODING) as safelist:
+                                            safelist.write(f"{tempNM} = {NOME_CURSO}/{NOME_MODULO}/{NOME_AULA}/aula-{index}.mp4\n")
+
+                                        aulaPath = f"Cursos/{NOME_CURSO}/ev/{tempNM}.mp4"
+                                        videosLongos += 1
+                                    if not os.path.isfile(aulaPath):
+                                        videoData = authMart.get(
+                                            f"{asset['url']}?{playerInfo['cloudFrontSignature']}")
+                                        masterPlaylist = m3u8.loads(
+                                            videoData.text)
+                                        res = []
+                                        highestQual = None
+                                        for playlist in masterPlaylist.playlists:
+                                            res.append(
+                                                playlist.stream_info.resolution)
+                                        res.sort(reverse=True)
+                                        for playlist in masterPlaylist.playlists:
+                                            if playlist.stream_info.resolution == res[0]:
+                                                highestQual = playlist.uri
+                                        if highestQual is not None:
+                                            videoData = authMart.get(
+                                                f"{asset['url'][:asset['url'].rfind('/')]}/{highestQual}?{playerInfo['cloudFrontSignature']}")
+                                            with open(f'{TEMP_FOLDER}/dump.m3u8', 'w') as dump:
+                                                dump.write(videoData.text)
+                                            videoPlaylist = m3u8.loads(
                                                 videoData.text)
-                                            res = []
-                                            highestQual = None
-                                            for playlist in masterPlaylist.playlists:
-                                                res.append(
-                                                    playlist.stream_info.resolution)
-                                            res.sort(reverse=True)
-                                            for playlist in masterPlaylist.playlists:
-                                                if playlist.stream_info.resolution == res[0]:
-                                                    highestQual = playlist.uri
-                                            if highestQual is not None:
-                                                videoData = aulagetter.get(
-                                                    f"{asset['url'][:asset['url'].rfind('/')]}/{highestQual}?{playerInfo['player']['cloudFrontSignature']}")
-                                                with open(f'{TEMP_FOLDER}/dump.m3u8', 'w') as dump:
-                                                    dump.write(videoData.text)
-                                                videoPlaylist = m3u8.loads(
-                                                    videoData.text)
-                                                key = videoPlaylist.segments[0].key.uri
-                                                totalSegmentos = videoPlaylist.segments[-1].uri.split(".")[
-                                                    0].split("-")[1]
-                                                for segment in videoPlaylist.segments:
-                                                    print(f"\r\tBaixando o segmento {Colors.Blue}{segment.uri.split('.')[0].split('-')[1]}{Colors.Reset}/{Colors.Magenta}{totalSegmentos}{Colors.Reset}!",
-                                                          end="", flush=True)
-                                                    uri = segment.uri
-                                                    frag = aulagetter.get(
-                                                        f"{asset['url'][:asset['url'].rfind('/')]}/{highestQual.split('/')[0]}/{uri}?{playerInfo['player']['cloudFrontSignature']}")
-                                                    with open(f"{TEMP_FOLDER}/" + uri, 'wb') as sfrag:
-                                                        sfrag.write(
-                                                            frag.content)
-                                                fragkey = aulagetter.get(
-                                                    f"{asset['url'][:asset['url'].rfind('/')]}/{highestQual.split('/')[0]}/{key}?{playerInfo['player']['cloudFrontSignature']}")
-                                                with open(f"{TEMP_FOLDER}/{key}", 'wb') as skey:
-                                                    skey.write(fragkey.content)
-                                                print(
-                                                    f"\r\tSegmentos baixados, gerando video final! {Colors.Red}(dependendo da config do pc este passo pode demorar até 20 minutos!){Colors.Reset}", end="\n", flush=True)
+                                            key = videoPlaylist.segments[0].key.uri
+                                            totalSegmentos = videoPlaylist.segments[-1].uri.split(".")[
+                                                0].split("-")[1]
+                                            for segment in videoPlaylist.segments:
+                                                print(f"\r\tBaixando o segmento {Colors.Blue}{segment.uri.split('.')[0].split('-')[1]}{Colors.Reset}/{Colors.Magenta}{totalSegmentos}{Colors.Reset}!",
+                                                        end="", flush=True)
+                                                uri = segment.uri
+                                                frag = authMart.get(
+                                                    f"{asset['url'][:asset['url'].rfind('/')]}/{highestQual.split('/')[0]}/{uri}?{playerInfo['cloudFrontSignature']}")
+                                                with open(f"{TEMP_FOLDER}/" + uri, 'wb') as sfrag:
+                                                    sfrag.write(
+                                                        frag.content)
+                                            fragkey = authMart.get(
+                                                f"{asset['url'][:asset['url'].rfind('/')]}/{highestQual.split('/')[0]}/{key}?{playerInfo['cloudFrontSignature']}")
+                                            with open(f"{TEMP_FOLDER}/{key}", 'wb') as skey:
+                                                skey.write(fragkey.content)
+                                            print(
+                                                f"\r\tSegmentos baixados, gerando video final! {Colors.Red}(dependendo da config do pc este passo pode demorar até 20 minutos!){Colors.Reset}", end="\n", flush=True)
 
-                                                # TODO Implementar verificação de hardware acceleration
-                                                # ffmpegcmd = f'ffmpeg -hide_banner -loglevel error -v quiet -stats -allowed_extensions ALL -hwaccel cuda -i {tempFolder}/dump.m3u8 -c:v h264_nvenc -n "{aulaPath}"'
+                                            # TODO Implementar verificação de hardware acceleration
+                                            # ffmpegcmd = f'ffmpeg -hide_banner -loglevel error -v quiet -stats -allowed_extensions ALL -hwaccel cuda -i {tempFolder}/dump.m3u8 -c:v h264_nvenc -n "{aulaPath}"'
 
-                                                ffmpegcmd = f'ffmpeg -hide_banner -loglevel error -v quiet -stats -allowed_extensions ALL -i {TEMP_FOLDER}/dump.m3u8 -n "{aulaPath}"'
+                                            ffmpegcmd = f'ffmpeg -hide_banner -loglevel error -v quiet -stats -allowed_extensions ALL -i {TEMP_FOLDER}/dump.m3u8 -n "{aulaPath}"'
 
-                                                if sys.platform.startswith('darwin'):
-                                                    # MacOs specific procedures
-                                                    subprocess.run(
-                                                        ffmpegcmd, shell=True)
-                                                elif sys.platform.startswith('win32'):
-                                                    # Windows specific procedures
-                                                    subprocess.run(ffmpegcmd)
+                                            if sys.platform.startswith('darwin'):
+                                                # MacOs specific procedures
+                                                subprocess.run(
+                                                    ffmpegcmd, shell=True)
+                                            elif sys.platform.startswith('win32'):
+                                                # Windows specific procedures
+                                                subprocess.run(ffmpegcmd)
 
-                                                # TODO Implementar verificação de falha pelo FFMPEG
-                                                # p = subprocess.run(ffmpegcmd)
-                                                # if p.returncode != 0:
-                                                #     pass
+                                            # TODO Implementar verificação de falha pelo FFMPEG
+                                            # p = subprocess.run(ffmpegcmd)
+                                            # if p.returncode != 0:
+                                            #     pass
 
-                                                vidCount += 1
-                                                print(
-                                                    f"Download da aula {Colors.Bold}{Colors.Magenta}{NOME_MODULO}/{NOME_AULA}{Colors.Reset} {Colors.Green}concluído{Colors.Reset}!")
-                                                time.sleep(3)
-                                                for ff in glob.glob(f"{TEMP_FOLDER}/*"):
-                                                    os.remove(ff)
-
-                                            else:
-                                                print(
-                                                    f"{Colors.Red}{Colors.Bold}Algo deu errado ao baixar a aula, redefinindo conexão para tentar novamente!{Colors.Reset}")
-                                                raise HTTPError
-                                        else:
-                                            print("VIDEO JA EXISTE")
                                             vidCount += 1
+                                            print(
+                                                f"Download da aula {Colors.Bold}{Colors.Magenta}{NOME_MODULO}/{NOME_AULA}{Colors.Reset} {Colors.Green}concluído{Colors.Reset}!")
+                                            time.sleep(3)
+                                            for ff in glob.glob(f"{TEMP_FOLDER}/*"):
+                                                os.remove(ff)
 
-                                    # tryDL = 0
+                                        else:
+                                            print(
+                                                f"{Colors.Red}{Colors.Bold}Algo deu errado ao baixar a aula, redefinindo conexão para tentar novamente!{Colors.Reset}")
+                                            raise HTTPError
+                                    else:
+                                        print("VIDEO JA EXISTE")
+                                        vidCount += 1
+
+                                # tryDL = 0
 
                         # Download de aula Externa
                         except KeyError:
                             try:
                                 fonteExterna = None
-                                pjson = BeautifulSoup(
-                                    infoAula['content'], features="html.parser")
+                                pjson = BeautifulSoup(infoAula['content'], features="html.parser")
                                 viframe = pjson.findAll("iframe")
-                                for x, i in enumerate(viframe, start=1):
+                                for index, i in enumerate(viframe, start=1):
                                     # TODO Mesmo trecho de aula longa zzz
 
-                                    filePath = os.path.dirname(
-                                        os.path.abspath(__file__))
-                                    aulaPath = f"{filePath}/Cursos/{NOME_CURSO}/{NOME_MODULO}/{NOME_AULA}/aula-{x}.mp4"
+                                    filePath = os.path.dirname(s.path.abspath(__file__))
+                                    aulaPath = f"{filePath}/Cursos/{NOME_CURSO}/{NOME_MODULO}/{NOME_AULA}/aula-{index}.mp4"
+
                                     if len(aulaPath) > 254:
                                         if not os.path.exists(f"Cursos/{NOME_CURSO}/ev"):
                                             os.makedirs(f"Cursos/{NOME_CURSO}/ev")
@@ -427,7 +436,7 @@ def baixarCurso(authMart, infoCurso, downloadAll):
                                             string.ascii_uppercase + string.digits, k=8))
                                         with open(f"Cursos/{NOME_CURSO}/ev/list.txt", "a", encoding=ENCODING) as safelist:
                                             safelist.write(
-                                                f"{tempNM} = {NOME_CURSO}/{NOME_MODULO}/{NOME_AULA}/aula-{x}.mp4\n")
+                                                f"{tempNM} = {NOME_CURSO}/{NOME_MODULO}/{NOME_AULA}/aula-{index}.mp4\n")
                                         aulaPath = f"Cursos/{NOME_CURSO}/ev/{tempNM}.mp4"
                                         videosLongos += 1
 
